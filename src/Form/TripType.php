618865e3f165ee2372a2097b55cda2caf1f572ed
Miguel Riesco <miguelrh.webdev@gmail.com>
2024-04-18 18:17:21 +0200

    create CRUD for trip

---
+++ src/Form/TripType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Drivers;
+use App\Entity\Trip;
+use App\Entity\Vehicles;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TripType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('vehicle', EntityType::class, [
+                'class' => Vehicles::class,
+'choice_label' => 'id',
+            ])
+            ->add('driver', EntityType::class, [
+                'class' => Drivers::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Trip::class,
+        ]);
+    }
+}
